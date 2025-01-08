444ba4b4e9f95e095a0fa0d5a8ab182ae60ce6b6
pleasant-star <kaisei1999410@gmail.com>
2025-01-09 04:21:36 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :nickname, presence: { message: 'を入力してください' }
+  validates :email, presence: { message: 'を入力してください' }, uniqueness: { message: 'はすでに登録されています' }
+  validates :password, presence: { message: 'を入力してください' }, length: { minimum: 6, message: 'は6文字以上で入力してください' }, confirmation: true
+  validates :last_name, presence: { message: 'を入力してください' },
+                        format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: { message: 'を入力してください' },
+                         format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name_kana, presence: { message: 'を入力してください' }, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :first_name_kana, presence: { message: 'を入力してください' }, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :birthday, presence: { message: 'を入力してください' }
 end
