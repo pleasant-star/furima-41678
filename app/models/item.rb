0d5d9cee4a253487525afacb2de063e43e912add
pleasant-star <kaisei1999410@gmail.com>
2025-01-10 18:42:34 +0900

    バリデーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :postage_id, presence: true
+  validates :area_id, presence: true
+  validates :send_day_id, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
 end
