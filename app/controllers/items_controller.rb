99e3b142cb795bf9ec961daa7a4bf08e9c49e82b
pleasant-star <kaisei1999410@gmail.com>
2025-02-13 13:59:43 +0900

    エラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :redirect_unless_authorized, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -26,8 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -35,5 +40,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id, :postage_id, :area_id, :send_day_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_authorized
+    return if current_user == @item.user && (!@item.respond_to?(:purchase) || @item.purchase.nil?)
+
+    redirect_to root_path
   end
 end
