ecd2cc7d99624d8fa63b1d39af14f6bf29c94e0d
pleasant-star <kaisei1999410@gmail.com>
2025-02-16 19:13:00 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,21 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user, :item, :zip_code, :area_id, :city, :address_line, :building_name, :tell, :purchase
+
+  with_options presence: true do
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address_line
+    validates :tell, format: { with: /\A\d{10,11}\z/, message: 'must be 10 or 11 digits' }
+    validates :user
+    validates :item
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user.id, item_id: item.id)
+
+    Address.create(zip_code: zip_code, area_id: area_id, city: city, address_line: address_line, building_name: building_name,
+                   tell: tell, purchase_id: purchase.id)
+  end
+end
