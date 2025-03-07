ef0726a8e9d54bd08372240c8bf6a1ef08d00ce4
pleasant-star <kaisei1999410@gmail.com>
2025-03-07 11:50:06 +0900

    クレジット決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,15 +3,19 @@ class PurchasesController < ApplicationController
   before_action :set_item
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +27,16 @@ class PurchasesController < ApplicationController
   end
 
   def purchase_params
-    params.permit(:zip_code, :area_id, :city, :address_line, :building_name, :tell)
-          .merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:zip_code, :area_id, :city, :address_line, :building_name, :tell)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_e9eb21d09dc8df8dbea09b8c'
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :zip_code, :area_id, :city, :address_line, :building_name, :tell
+  attr_accessor :user_id, :item_id, :zip_code, :area_id, :city, :address_line, :building_name, :tell, :token
 
   with_options presence: true do
     validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
